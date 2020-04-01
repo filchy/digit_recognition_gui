14c8f104aa382fd2927d73f7b6f7c9e3ee9d0f00
chyfil <54070973+filchy@users.noreply.github.com>
2020-04-01 20:15:43 +0200

    Update vgg16_model.py

--- vgg16_model.py
+++ vgg16_model.py
@@ -5,10 +5,10 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Dr
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import ReduceLROnPlateau
 
-from load_data import load
+from load_data import tensor_load
 
 
-X_train1, X_test1, y_train1, y_test1 = load()
+X_train1, X_test1, y_train1, y_test1 = tensor_load()
 
 opt = Adam(lr=0.001)
 
