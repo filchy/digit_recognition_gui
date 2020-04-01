7e51c9b4d84739ba33e59fd5df72df1479d3acc5
chyfil <54070973+filchy@users.noreply.github.com>
2020-04-01 20:14:17 +0200

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 
@@ -18,4 +19,21 @@ def load():
 
 	X_train1, X_test1, y_train1, y_test1 = train_test_split(X_train, y_train, test_size = 0.15)
 
+	return X_train1, X_test1, y_train1, y_test1
+
+
+def tensor_load():
+	mnist = tf.keras.datasets.mnist
+	(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+	X_train = tf.keras.utils.normalize(X_train, axis=1)
+	X_test = tf.keras.utils.normalize(X_test, axis=1)
+
+	X_train = X_train.astype("float32")/255
+	y_train = to_categorical(y_train, num_classes=10)
+
+	X_train = np.array(X_train).reshape(-1, 28, 28 ,1)
+
+	X_train1, X_test1, y_train1, y_test1 = train_test_split(X_train, y_train, test_size = 0.15)
+	
 	return X_train1, X_test1, y_train1, y_test1
